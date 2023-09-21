901ef085f5bb0cf0bb2d149bafd2b42a86e46e53
Febrianto752 <febrianto.bekasi@gmail.com>
2023-09-21 10:28:45 +0700

    feat: add initial migration

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BulkyWeb.Models;
+
+public class Category
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Required]
+    [Column("name")]
+    public string Name { get; set; }
+
+    [Column("display_order")]
+    public string DisplayOrder { get; set; }
+
+    [Column("created_date")]
+    public DateTime CreatedDate { get; set; }
+
+    [Column("modified_date")]
+    public DateTime ModifiedDate { get; set; }
+}
+
